39f5b529c894fea136ce99c1e93c4ee4adfb4924
cPaletta <krzkaleta07@gmail.com>
2025-01-20 21:49:07 +0100

    ready

--- Histogram_test.java
+++ Histogram_test.java
@@ -5,30 +5,41 @@ class Histogram_test {
     
     public static void main(String[] args) {
 
-	Scanner scanner = new Scanner(System.in);
-	
-	System.out.println("Set image size: n (#rows), m(#kolumns)");
-	int n = scanner.nextInt();
-	int m = scanner.nextInt();
-	Obraz obraz_1 = new Obraz(n, m);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Set image size: n (#rows), m(#kolumns)");
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		Obraz obraz_1 = new Obraz(n, m);
 
-	obraz_1.calculate_histogram();
-	obraz_1.print_histogram();
+		obraz_1.calculate_histogram();
+		obraz_1.print_histogram();
 
-	// System.out.println("Set number of threads");
-	// int num_threads = scanner.nextInt();
+		System.out.println("Set number of threads");
+		int num_threads = scanner.nextInt();
 
-	// Watek[] NewThr = new Watek[num_threads];
-    
-	// for (int i = 0; i < num_threads; i++) {
-	//     (NewThr[i] = new Watek(...,obraz_1)).start();
-	// }
-    
-	// for (int i = 0; i < num_threads; i++) {
-	//     try {
-	// 	NewThr[i].join();
-	//     } catch (InterruptedException e) {}
-	// }
+		Thread[] NewThr = new Thread[num_threads];
+		int range = 94 / num_threads;
+
+		for (int i = 0; i < num_threads; i++) {
+			int znakStart = i * range; 
+			int znakEnd;
+		
+			if (i == num_threads - 1) {
+				znakEnd = 94;
+			} else {
+				znakEnd = znakStart + range - 1;
+			}
+		
+			NewThr[i] = new Watek(znakStart, znakEnd, obraz_1); // Tworzenie wątku
+			NewThr[i].start();
+		}
+		
+		for (int i = 0; i < num_threads; i++) {
+		    try {
+			NewThr[i].join();
+		    } catch (InterruptedException e) {}
+		}
 
     }
 
